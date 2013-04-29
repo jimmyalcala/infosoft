b67cf969192a3939edb910c23b1e4a30abafe794
Jimmy Alcala <jimmyalcala@gmail.com>
2013-04-29 14:28:53 -0430

    Arreglos de Static

--- infosoft/settings.py
+++ infosoft/settings.py
@@ -1,5 +1,10 @@
 # Django settings for infosoft project.
 # -*- coding: utf-8 -*-
+import os
+
+RUTA_PROYECTO = os.path.dirname(os.path.realpath(__file__))
+
+
 from local_settings import *
 
 SITE_ID = 1
@@ -17,7 +22,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = os.path.join(RUTA_PROYECTO,'static_media/uploads/')
+MEDIA_ROOT = os.path.join(RUTA_PROYECTO,'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -28,7 +33,7 @@ MEDIA_URL = '/uploads/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(RUTA_PROYECTO,'static')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -39,7 +44,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    os.path.join(RUTA_PROYECTO,'static_media/static'),
+    #os.path.join(RUTA_PROYECTO,'static_media/static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -50,8 +55,6 @@ STATICFILES_FINDERS = (
 #    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = 'fjdf74!w0^)372$x72w-9m@aidff$heaas+xgw_-2666+ky)hn'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
